98670ac8c974a7c907e3e61def49acdf8942fb67
Thijs Brobbel <t.brobbel@coolblue.nl>
2017-02-08 10:53:36 +0100

    Add basic scaffolding for running the app.

--- Dcc/Program.cs
+++ Dcc/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace Tiesmaster.Dcc
+﻿using System.IO;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace Tiesmaster.Dcc
 {
     public static class Program
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                 .AddCommandLine(args)
+                 .AddEnvironmentVariables(prefix: "DCC_")
+                 .Build();
+
+            var host = new WebHostBuilder()
+                .UseConfiguration(config)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
         }
     }
 }
