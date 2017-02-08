d82aad1836c1ca5f9f079f970b2e1786dd146031
Thijs Brobbel <t.brobbel@coolblue.nl>
2017-02-08 10:40:33 +0100

    Make Tiesmaster.Dcc the default namespace,
    and cleanup Program.cs.

--- Dcc/Program.cs
+++ Dcc/Program.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Dcc
+﻿namespace Tiesmaster.Dcc
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
         }
     }
