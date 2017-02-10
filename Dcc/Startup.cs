c83388c7d091b4d3099f1218ed9c74f4ae6b83d8
Thijs Brobbel <t.brobbel@coolblue.nl>
2017-02-10 08:39:30 +0100

    Copy in Proxy code from ProxyMiddleware.cs,
    from ASP.NET Core Proxy package [1].
    
    [1] https://raw.githubusercontent.com/aspnet/Proxy/dev/src/Microsoft.AspNetCore.Proxy/ProxyMiddleware.cs

--- Dcc/Startup.cs
+++ Dcc/Startup.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -23,7 +25,46 @@ namespace Tiesmaster.Dcc
 
         private static async Task RunDccProxyAsync(HttpContext context)
         {
-            await context.Response.WriteAsync("Hello from DCC ;)");
-        }
+            var requestMessage = new HttpRequestMessage();
+            var requestMethod = context.Request.Method;
+            if(!HttpMethods.IsGet(requestMethod) &&
+                !HttpMethods.IsHead(requestMethod) &&
+                !HttpMethods.IsDelete(requestMethod) &&
+                !HttpMethods.IsTrace(requestMethod))
+            {
+                var streamContent = new StreamContent(context.Request.Body);
+                requestMessage.Content = streamContent;
+            }
+
+            // Copy the request headers
+            foreach(var header in context.Request.Headers)
+            {
+                if(!requestMessage.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray()) && requestMessage.Content != null)
+                {
+                    requestMessage.Content?.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray());
+                }
+            }
+
+            requestMessage.Headers.Host = _options.Host + ":" + _options.Port;
+            var uriString = $"{_options.Scheme}://{_options.Host}:{_options.Port}{context.Request.PathBase}{context.Request.Path}{context.Request.QueryString}";
+            requestMessage.RequestUri = new Uri(uriString);
+            requestMessage.Method = new HttpMethod(context.Request.Method);
+            using(var responseMessage = await _httpClient.SendAsync(requestMessage, HttpCompletionOption.ResponseHeadersRead, context.RequestAborted))
+            {
+                context.Response.StatusCode = (int)responseMessage.StatusCode;
+                foreach(var header in responseMessage.Headers)
+                {
+                    context.Response.Headers[header.Key] = header.Value.ToArray();
+                }
+
+                foreach(var header in responseMessage.Content.Headers)
+                {
+                    context.Response.Headers[header.Key] = header.Value.ToArray();
+                }
+
+                // SendAsync removes chunking from the response. This removes the header so it doesn't expect a chunked response.
+                context.Response.Headers.Remove("transfer-encoding");
+                await responseMessage.Content.CopyToAsync(context.Response.Body);
+            }
     }
 }
