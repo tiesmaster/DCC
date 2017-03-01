31b0ca92de0fbcb270c61d94dce4179b609033ad
Thijs Brobbel <thijsbrobbel@gmail.com>
2017-03-01 21:33:30 +0100

    Add simple test.

---
+++ Dcc.Test/DccMiddlewareTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Dcc.Test
+{
+    public class DccMiddlewareTests
+    {
+        [Fact]
+        public void SanityTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
