00717b2b3617df5c7b1cbf2344a0846fd911a371
Thijs Brobbel <thijsbrobbel@gmail.com>
2017-03-01 22:08:13 +0100

    Add basic tests of middleware.

--- Dcc.Test/DccMiddlewareTests.cs
+++ Dcc.Test/DccMiddlewareTests.cs
@@ -1,8 +1,14 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
 using FluentAssertions;
 
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Options;
 
 using Tiesmaster.Dcc;
@@ -26,7 +32,95 @@ namespace Dcc.Test
             sut.Should().NotBeNull();
         }
 
+        [Fact]
+        public async Task FirstInvokeWillProxyRequest()
+        {
+            // arrange
+            var expectedResponse = Guid.NewGuid().ToString();
+
+            var builder = new WebHostBuilder()
+                .Configure(app =>
+                {
+                    app.RunDcc(
+                        new DccOptions
+                        {
+                            Host = "localhost",
+                            Port = "1234",
+                            BackChannelMessageHandler = new TestMessageHandler
+                                                        {
+                                                            Sender = req =>
+                                                            {
+                                                                var response = new HttpResponseMessage(HttpStatusCode.OK);
+                                                                response.Content = new StringContent(expectedResponse);
+                                                                return response;
+                                                            }
+                                                        }
+                        });
+
+                });
+            var server = new TestServer(builder);
+
+            // act
+            var result = await server.CreateClient().GetStringAsync("test");
+
+            // assert
+            result.Should().Be(expectedResponse);
+        }
+        [Fact]
+        public async Task SecondInvokeWillReturnStoredTape()
+        {
+            // arrange
+            var expectedResponse = Guid.NewGuid().ToString();
+            var invocationCount = 0;
+
+            var builder = new WebHostBuilder()
+                .Configure(app =>
+                {
+                    app.RunDcc(
+                        new DccOptions
+                        {
+                            Host = "localhost",
+                            Port = "1235",
+                            BackChannelMessageHandler = new TestMessageHandler
+                            {
+                                Sender = req =>
+                                {
+                                    invocationCount++;
+                                    var response = new HttpResponseMessage(HttpStatusCode.OK);
+                                    response.Content = new StringContent(expectedResponse);
+                                    return response;
+                                }
+                            }
+                        });
+
+                });
+            var server = new TestServer(builder);
+            await server.CreateClient().GetStringAsync("test");
+
+            // act
+            var result = await server.CreateClient().GetStringAsync("test");
+
+            // assert
+            invocationCount.Should().Be(1);
+        }
+
         private static IOptions<DccOptions> CreateOptions() => Options.Create(new DccOptions {Host = "test"});
         private Task NoOpNext(HttpContext context) => Task.CompletedTask;
+
+        private class TestMessageHandler : HttpMessageHandler
+        {
+            public Func<HttpRequestMessage, HttpResponseMessage> Sender { get; set; }
+
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                if(Sender != null)
+                {
+                    return Task.FromResult(Sender(request));
+                }
+
+                return Task.FromResult<HttpResponseMessage>(null);
+            }
+        }
+
     }
 }
